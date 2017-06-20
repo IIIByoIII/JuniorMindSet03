33b7297be7d521e30e559cf081a3796aad4c81f7
IIIByoIII <robertkiss26@gmail.com>
2017-06-20 21:16:37 +0300

    Added tests from size 4 to size 8

--- ChessBoardSquares/ChessBoardSquaresTests.cs
+++ ChessBoardSquares/ChessBoardSquaresTests.cs
@@ -24,6 +24,36 @@ namespace ChessBoardSquares
       Assert.AreEqual(14, TotalNumberOfSquares(3));
     }
 
+    [TestMethod]
+    public void SquaresForSizeFour()
+    {
+      Assert.AreEqual(30, TotalNumberOfSquares(4));
+    }
+
+    [TestMethod]
+    public void SquaresForSizeFive()
+    {
+      Assert.AreEqual(55, TotalNumberOfSquares(5));
+    }
+
+    [TestMethod]
+    public void SquaresForSizeSix()
+    {
+      Assert.AreEqual(91, TotalNumberOfSquares(6));
+    }
+
+    [TestMethod]
+    public void SquaresForSizeSeven()
+    {
+      Assert.AreEqual(140, TotalNumberOfSquares(7));
+    }
+
+    [TestMethod]
+    public void SquaresForSizeEight()
+    {
+      Assert.AreEqual(204, TotalNumberOfSquares(8));
+    }
+
     int TotalNumberOfSquares (int sizeOfBoard)
     {
       int totalSquares = 0;
