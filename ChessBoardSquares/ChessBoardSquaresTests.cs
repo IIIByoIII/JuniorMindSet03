0a9a4425cf8b4d94fcc26a3507329966a8173186
IIIByoIII <robertkiss26@gmail.com>
2017-06-20 19:51:10 +0300

    Added SquaresForSizeOne test

--- ChessBoardSquares/ChessBoardSquaresTests.cs
+++ ChessBoardSquares/ChessBoardSquaresTests.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ChessBoardSquares
 {
   [TestClass]
-  public class UnitTest1
+  public class ChessBoardSquaresTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void SquaresForSizeOne()
     {
+      Assert.AreEqual(1, TotalNumberOfSquares(1));
+    }
+
+    int TotalNumberOfSquares (int sizeOfBoard)
+    {
+      int totalSquares = sizeOfBoard * sizeOfBoard;
+      return totalSquares;
     }
   }
 }
