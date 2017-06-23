ee78948dfbad76ee1a8691072c9cd52398a1e4b3
IIIByoIII <robertkiss26@gmail.com>
2017-06-23 21:50:55 +0300

    Added ColumnAF test

--- ExcelColumns/ExcelColumnsTests.cs
+++ ExcelColumns/ExcelColumnsTests.cs
@@ -12,9 +12,29 @@ namespace ExcelColumns
       Assert.AreEqual("F", ExcelColumn(6));
     }
 
+    [TestMethod]
+    public void ColumnAF()
+    {
+      Assert.AreEqual("AF", ExcelColumn(32));
+    }
+
+    string allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     string ExcelColumn (int columnNumber)
     {
-      string allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string columnLetter = "";
+      while (columnNumber > 26)
+      {
+        int columnQuotient = columnNumber / 26;
+        int columnRemainder = columnNumber % 26;
+        columnLetter += CurrentColumn(columnQuotient);
+        columnNumber = columnRemainder;
+      }
+      return columnLetter += CurrentColumn(columnNumber);
+    }
+
+    string CurrentColumn (int columnNumber)
+    {
       return allLetters[columnNumber - 1].ToString();
     }
   }
