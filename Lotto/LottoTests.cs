81b481f7feaf2f73b6bb0c5a0920c7ad5587ec5a
IIIByoIII <robertkiss26@gmail.com>
2017-06-29 20:06:53 +0300

    Added CategoryThree6of49 test
    Modified name of previous tests to be more specific

--- Lotto/LottoTests.cs
+++ Lotto/LottoTests.cs
@@ -13,15 +13,21 @@ namespace Lotto
     }
 
     [TestMethod]
-    public void CategoryOne()
+    public void CategoryOne6of49()
     {
       Assert.AreEqual(13983816d, CalculateChance(6, 49), 1d);
     }
 
     [TestMethod]
-    public void CategoryTwo()
+    public void CategoryTwo6of49()
     {
       Assert.AreEqual(54200.8d, WinningChance(5, 6, 49), 1d);
+    }
+
+    [TestMethod]
+    public void CategoryThree6of49()
+    {
+      Assert.AreEqual(1032.4d, WinningChance(4, 6, 49), 1d);
     }
 
     double Factorial (double number)
