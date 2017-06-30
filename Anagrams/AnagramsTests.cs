d9a2638d4fc56a660067866728a675c4092fa3b3
IIIByoIII <robertkiss26@gmail.com>
2017-06-30 21:24:35 +0300

    Added AnagramsForThreeUnique test

--- Anagrams/AnagramsTests.cs
+++ Anagrams/AnagramsTests.cs
@@ -4,11 +4,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Anagrams
 {
   [TestClass]
-  public class UnitTest1
+  public class AnagramsTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void AnagramsForThreeUnique()
     {
+      Assert.AreEqual(6d, AvailableAnagrams("ABC"));
+    }
+
+    double Factorial (double number)
+    {
+      double factorialForNumber = number;
+      for (double i = number; i > 2; i--)
+      {
+        factorialForNumber *= i-1;
+      }
+      return factorialForNumber;
+    }
+
+    double AvailableAnagrams(string inputWord)
+    {
+      double inputWordLength = inputWord.Length;
+      return Factorial(inputWordLength);
     }
   }
 }
