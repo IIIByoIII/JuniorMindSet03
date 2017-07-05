0aafc65aa991abf69d361c7fbddfbe63dd424226
IIIByoIII <robertkiss26@gmail.com>
2017-07-05 18:50:21 +0300

    Added NextMeeting6to4 test

--- LunchMeeting/LunchMeetingTests.cs
+++ LunchMeeting/LunchMeetingTests.cs
@@ -7,8 +7,21 @@ namespace LunchMeeting
   public class LunchMeetingTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void NextMeeting6to4()
     {
+      Assert.AreEqual(12, NextMeeting(6, 4));
+    }
+
+    int NextMeeting (int yourDays, int friendsDays)
+    {
+      int maxDays = yourDays > friendsDays ? yourDays : friendsDays;
+      int minDays = yourDays < friendsDays ? yourDays : friendsDays;
+      for (int i = 1; i < minDays; i++)
+      {
+        if (0d == ((maxDays * i * 1d) % minDays))
+          return maxDays * i;
+      }
+      return maxDays * minDays;
     }
   }
 }
