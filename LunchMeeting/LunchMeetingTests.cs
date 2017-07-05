bf49b641a7692343fe76df04677dc9d21258c34d
IIIByoIII <robertkiss26@gmail.com>
2017-07-05 18:24:37 +0300

    Created new LunchMeeting project

---
+++ LunchMeeting/LunchMeetingTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LunchMeeting
+{
+  [TestClass]
+  public class LunchMeetingTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
---
+++ LunchMeeting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LunchMeeting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LunchMeeting")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e584c4e-1e90-4e00-a6e4-4ab3f5c2c986")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
