a91168edd5f61e6266e06a52ed405805b61b427d
IIIByoIII <robertkiss26@gmail.com>
2017-07-12 16:58:16 +0300

    Created new BaseTwoOperations project

---
+++ BaseTwoOperations/BaseTwoOperationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseTwoOperations
+{
+  [TestClass]
+  public class BaseTwoOperationTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
---
+++ BaseTwoOperations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BaseTwoOperations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BaseTwoOperations")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("255f13cd-28c8-45fd-8839-016789db0c04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
